e8ef7ad2001577869c32d24db3ccb299afb5ee69
elasticsounds <elasticsounds@gmail.com>
2024-10-02 18:24:19 -0400

    Commit tailwind compiled css and config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './*.html',           // Looks at HTML files in the root folder
     './assets/**/*.html',  // Looks at HTML files in the "assets" folder
+    './assets/**/*.js',    // Looks at JS files in the "assets" folder
 ],
   theme: {
     extend: {},
